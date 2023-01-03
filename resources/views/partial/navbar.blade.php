47e48672ead5064d49b39c219b3464a2725065bf
Usein <useinakbar01@gmail.com>
2023-01-04 06:51:27 +0700

    adding navbar navigation

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="nav">
     <img src="asset/logo.svg" alt="Dunia Tani" class="nav__logo">
     <div class="nav__box">
-        <a href="" class="nav__link">Home</a>
+        <a href="/" class="nav__link">Home</a>
         <a href="" class="nav__link">Events</a>
         <a href="" class="nav__link">Community</a>
-        <a href="" class="nav__link">Courses</a>
+        <a href="/course" class="nav__link">Courses</a>
         <a href="" class="nav__link nav-redirect">Become a Guru Tani</a>
         <a href="/login" class="nav__link nav-btn">Login</a>
         {{-- <div class="dropdown" id="">
