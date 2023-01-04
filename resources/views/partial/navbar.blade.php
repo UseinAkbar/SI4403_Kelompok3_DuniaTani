a7b4aeb6c0dcf12ee3c53cdc7c72d11f11051086
Fadli I <93766012+Izurohmanq@users.noreply.github.com>
2023-01-04 07:12:25 +0700

    fixing navbar course

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,23 +1,36 @@
 <nav class="nav">
     <img src="asset/logo.svg" alt="Dunia Tani" class="nav__logo">
     <div class="nav__box">
-        <a href="" class="nav__link">Home</a>
+        <a href="/" class="nav__link">Home</a>
         <a href="" class="nav__link">Events</a>
         <a href="" class="nav__link">Community</a>
-        <a href="" class="nav__link">Courses</a>
+        <a href="/course" class="nav__link">Courses</a>
         <a href="" class="nav__link nav-redirect">Become a Guru Tani</a>
-        <a href="/login" class="nav__link nav-btn">Login</a>
-        {{-- <div class="dropdown" id="">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        @auth
+        <div class="dropdown" id="">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
                 <img src="asset/avatar.png" alt="">
-                Deva Mahendra
+                {{ auth()->user()->username }}
             </button>
             <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="">My Order</a></li>
                 <li><a class="dropdown-item" href="">My Course</a></li>
                 <li><a class="dropdown-item" href="">Edit Profile</a></li>
-                <li><a class="dropdown-item" href="">Logout</a></li>
+                <li>
+                    <form action="/logout" method="post">
+                        @csrf
+
+                        <button type='submit' class='dropdown-item'>
+                            <i>Logout</i>
+                        </button>
+                    </form>
+                </li>
             </ul>
-        </div> --}}
+        </div>
+        @else
+        <a href="/login" class="nav__link nav-btn">Login</a>
+        @endauth
+
     </div>
 </nav>
